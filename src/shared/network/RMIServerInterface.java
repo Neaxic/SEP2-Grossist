71c148c20ca5a8d764b9cdc3758f937befac3159
Young <andreasyoungandersen@gmail.com>
2021-03-31 16:06:50 +0200

    Metoder rettet og info oprettet

--- src/shared/network/RMIServerInterface.java
+++ src/shared/network/RMIServerInterface.java
@@ -3,6 +3,20 @@ package shared.network;
 import java.rmi.Remote;
 
 public interface RMIServerInterface extends Remote {
+
+    /**
+     * @return Den ene værdi der er gemt på serveren
+     */
     int getThingFromServer();
-    void setThingOnServer();
+
+    /**
+     * @param val Den værdi du vil sætte på serveren
+     */
+    void setThingOnServer(int val);
+
+
+    /**
+     * @return Hvis der kan connectes, så vil værdien være True
+     */
+    boolean ping();
 }
