7ca05db41e8900a4d7bbcbe25460af737838f718
Firrez <246299@via.dk>
2021-04-01 17:36:33 +0200

    Updated Risk-Test

--- src/server/model/RISKASSESSPROTO.java
+++ src/server/model/RISKASSESSPROTO.java
@@ -5,6 +5,7 @@ import shared.wares.Produce;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 
 public class RISKASSESSPROTO
 {
@@ -26,5 +27,32 @@ public class RISKASSESSPROTO
     }
 
     return new RiskReport("green", "Everything is in order.");
+  }
+
+  public static ArrayList<RiskReport> massAssess(ArrayList<Produce> items)
+  {
+    ArrayList<RiskReport> reports = new ArrayList<>();
+    LocalDate today = LocalDate.now(ZoneId.systemDefault());
+
+    for (Produce item : items)
+    {
+      long daysToBb = ChronoUnit.DAYS.between(today, item.getBb());
+      double daysOfStock = (double) item.getStock() / item.getSoldDaily();
+
+      if (item.getDeliveryDays() >= daysOfStock)
+      {
+        reports.add(new RiskReport("yellow", "Stock too low, order new produce."));
+      }
+      else if (daysOfStock > daysToBb)
+      {
+        reports.add(new RiskReport("red", "Too much stock of produce."));
+      }
+      else
+      {
+        reports.add(new RiskReport("green", "Everything is in order."));
+      }
+    }
+
+    return reports;
   }
 }
--- src/server/model/RISKTEST.java
+++ src/server/model/RISKTEST.java
@@ -4,6 +4,7 @@ import shared.wares.Produce;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 public class RISKTEST
 {
@@ -11,9 +12,28 @@ public class RISKTEST
   {
     DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
     LocalDate bb1 = LocalDate.parse("08/04/2021", dtf);
+    LocalDate bb2 = LocalDate.parse("16/04/2021", dtf);
+    LocalDate bb3 = LocalDate.parse("01/05/2021", dtf);
+    LocalDate bb4 = LocalDate.parse("26/11/2021", dtf);
     Produce tomato = new Produce(2000, 200, 5, 20, bb1);
+    Produce tinOfBeans = new Produce(20, 5, 10, 0, bb4);
+    Produce milk = new Produce(5000, 1000, 2, 20, bb2);
+    Produce cheese = new Produce(4000, 300, 3, 20, bb3);
+
+    ArrayList<Produce> items = new ArrayList<>();
+    items.add(tomato);
+    items.add(tinOfBeans);
+    items.add(milk);
+    items.add(cheese);
 
     RiskReport riskReport = RISKASSESSPROTO.riskAssess(tomato);
-    System.out.println(riskReport.getSeverity() + ": " + riskReport.getMessage());
+    System.out.println("Single Test: " + riskReport.getSeverity() + ": " + riskReport.getMessage());
+
+    ArrayList<RiskReport> reports = RISKASSESSPROTO.massAssess(items);
+
+    for (RiskReport massReport : reports)
+    {
+      System.out.println("Mass Test: " + massReport.getSeverity() + ": " + massReport.getMessage());
+    }
   }
 }
