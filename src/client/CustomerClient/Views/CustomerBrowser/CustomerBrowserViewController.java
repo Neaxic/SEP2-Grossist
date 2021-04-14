1731b0bd9aee4db23b8409d3d8046c9c506d2850
Young <andreasyoungandersen@gmail.com>
2021-04-14 11:29:40 +0200

    Flere opdateringer

--- src/client/CustomerClient/Views/CustomerBrowser/CustomerBrowserViewController.java
+++ src/client/CustomerClient/Views/CustomerBrowser/CustomerBrowserViewController.java
@@ -3,9 +3,11 @@ package client.CustomerClient.Views.CustomerBrowser;
 import client.CustomerClient.Views.CustomerViewController;
 import client.CustomerClient.Views.ViewModel;
 import client.core.ViewHandler;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
@@ -21,60 +23,59 @@ public class CustomerBrowserViewController implements CustomerViewController {
 	// Item List for Product Population
 	@FXML private VBox productItemList;
 
+	private ViewHandler viewHandler;
+	private CustomerBrowserViewModel viewModel;
+
+	private Object[] activeItemList = new Object[2]; // FIXME: Needs to be of the Object, used to create a Product
+
 	public void tempMethodForSettingController() {
 		System.out.println("duh");
 	}
 
 	@Override
 	public void init(ViewHandler viewHandler, ViewModel viewModel) {
-		// TODO
-	}
-
-	@Override
-	public void swapScene(String sceneName) {
-		// TODO
+		this.viewHandler = viewHandler;
+		this.viewModel = (CustomerBrowserViewModel) viewModel;
+		loadAllProducts();
 	}
 
 	public void loadAllProducts() {
+		viewModel.loadAllProducts();
 		// TODO: Load All Products from the Database
-		// productItemList needs to be populated
 	}
 
 	public void loadSpecificCategory(MouseEvent mouseEvent) {
 		Node selected = mouseEvent.getPickResult().getIntersectedNode();
-		if (selected instanceof Text){
-			System.out.println(((Text) selected).getText()); // Should return text as a string
+		if (selected instanceof Text) {
+			String category = ((Text) selected).getText();
+			System.out.println(category); // Should return text as a string
+			populate(category);
 		}
 	}
 
-	private void populate(String category){
-		switch (category.toLowerCase()){
-			case "all":
-				// TODO: Implement Population
-				return;
-			case "colonial":
-				// TODO: Implement Population
-				return;
-			case "diary and eggs":
-				// TODO: Implement Population
-				return;
-			case "frozen food":
-				// TODO: Implement Population
-				return;
-			case "fruits and vegetables":
-				// TODO: Implement Population
-				return;
-			case "meat and seafood":
-				// TODO: Implement Population
-				return;
-			case "beverages (non-alcohol)":
-				// TODO: Implement Population
-				return;
-			case "beverages (alcohol)":
-				// TODO: Implement Population
-				return;
-			default:
-				throw new IllegalArgumentException("Invalid Category for Population of Items");
+	private void populate(String category) {
+		viewModel.populate(category);
+		for (Object item : activeItemList) { // FIXME: Needs to be of the Object, used to create a Product
+			productItemList.getChildren().add(createEntry(item));
 		}
+	}
+
+	private HBox createEntry(Object item){ // FIXME: Needs to be of the Object, used to create a Product
+		HBox entry = new HBox();
+		return entry;
+	}
+
+	// SCENE MANAGING
+	@Override
+	public void swapScene(String sceneName) {
+		viewHandler.openView(sceneName);
+	}
+
+	public void openBasket() {
+		swapScene("basket");
+	}
+
+	public void openSubscriptions() {
+		swapScene("subscriptions");
 	}
 }
--- src/client/CustomerClient/Views/CustomerBrowser/CustomerBrowserViewModel.java
+++ src/client/CustomerClient/Views/CustomerBrowser/CustomerBrowserViewModel.java
@@ -2,6 +2,42 @@ package client.CustomerClient.Views.CustomerBrowser;
 
 import client.CustomerClient.Views.ViewModel;
 
-public class CustomerBrowserViewModel implements ViewModel
-{
+public class CustomerBrowserViewModel implements ViewModel {
+
+
+	public void populate(String category) {
+		switch (category.toLowerCase()) {
+			case "all":
+				// TODO: Implement Population
+				return;
+			case "colonial":
+				// TODO: Implement Population
+				return;
+			case "diary and eggs":
+				// TODO: Implement Population
+				return;
+			case "frozen food":
+				// TODO: Implement Population
+				return;
+			case "fruits and vegetables":
+				// TODO: Implement Population
+				return;
+			case "meat and seafood":
+				// TODO: Implement Population
+				return;
+			case "beverages (non-alcohol)":
+				// TODO: Implement Population
+				return;
+			case "beverages (alcohol)":
+				// TODO: Implement Population
+				return;
+			default:
+				throw new IllegalArgumentException("Invalid Category for Population of Items");
+		}
+	}
+
+	public void loadAllProducts() {
+		// Load products from Database
+		populate("all");
+	}
 }
