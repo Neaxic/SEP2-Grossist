c31079ee7974f7a13f234c59ff7a7c6bdbe6b92f
Firrez <246299@via.dk>
2021-04-14 12:26:34 +0200

    Updated RunApp & GrosserApp

--- src/client/GrosserApp.java
+++ src/client/GrosserApp.java
@@ -1,5 +1,21 @@
 package client;
 
-public class GrosserApp
+import client.core.CustomerViewHandler;
+import client.core.ViewHandler;
+import client.core.ViewModelFactory;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GrosserApp extends Application
 {
+  @Override
+  public void start(Stage stage) throws Exception {
+    ClientFactory clientFactory = new ClientFactory();
+    ModelFactory modelFactory = new ModelFactory(clientFactory);
+    ViewModelFactory viewModelFactory = new ViewModelFactory(modelFactory);
+    ViewHandler viewHandler = new CustomerViewHandler(stage, viewModelFactory);
+
+    viewHandler.start();
+  }
+
 }
--- src/client/RunApp.java
+++ src/client/RunApp.java
@@ -1,20 +1,11 @@
 package client;
 
-import client.core.CustomerViewHandler;
-import client.core.ViewHandler;
-import client.core.ViewModelFactory;
 import javafx.application.Application;
-import javafx.stage.Stage;
 
-public class RunApp extends Application {
-
-	@Override
-	public void start(Stage stage) throws Exception {
-		ClientFactory clientFactory = new ClientFactory();
-		ModelFactory modelFactory = new ModelFactory(clientFactory);
-		ViewModelFactory viewModelFactory = new ViewModelFactory(modelFactory);
-		ViewHandler viewHandler = new CustomerViewHandler(stage, viewModelFactory);
-
-		viewHandler.start();
+public class RunApp
+{
+	public static void main(String[] args)
+	{
+		Application.launch(GrosserApp.class);
 	}
 }
