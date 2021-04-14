f65dbc2a41606641b84d9026b08f5c1d633fca90
Young <andreasyoungandersen@gmail.com>
2021-04-14 09:52:22 +0200

    ViewController forbundet

--- src/client/CustomerClient/Views/CustomerBrowser/CustomerBrowserViewController.java
+++ src/client/CustomerClient/Views/CustomerBrowser/CustomerBrowserViewController.java
@@ -2,4 +2,7 @@ package client.CustomerClient.Views.CustomerBrowser;
 
 public class CustomerBrowserViewController
 {
+	public void tempMethodForSettingController(){
+		System.out.println("duh");
+	}
 }
