62d8796f6fb0aa95907de85df4a8260f5715d817
Young <andreasyoungandersen@gmail.com>
2021-04-20 07:35:50 +0200

    Javadoc for Constructor lavet

--- src/shared/wares/Product.java
+++ src/shared/wares/Product.java
@@ -4,20 +4,27 @@ import javafx.util.Pair;
 
 import java.time.LocalDate;
 
-public abstract class Product
-{
-  private Pair<Integer, String> stock;
-  private Pair<Double, String> price;
-  private int soldDaily, deliveryDays;
-  private LocalDate bb;
+public abstract class Product {
+	private Pair<Integer, String> stock;
+	private Pair<Double, String> price;
+	private int soldDaily, deliveryDays;
+	private LocalDate bb;
 
-  public Product(Pair<Integer, String> stock, int soldDaily, int deliveryDays, Pair<Double, String> price,
-      LocalDate bb)
-  {
-    this.stock = stock;
-    this.soldDaily = soldDaily;
-    this.deliveryDays = deliveryDays;
-    this.price = price;
-    this.bb = bb;
-  }
+	/**
+	 * Constructor for a generic product item
+	 *
+	 * @param stock        Amount and measurement type ex. <500, kg>
+	 * @param soldDaily    Amount of product sold on average each day
+	 * @param deliveryDays Expected time before product will be ready and delivered
+	 * @param price        Price of product and of what measurement type
+	 * @param bb           Best Before Date of Product
+	 */
+	public Product(Pair<Integer, String> stock, int soldDaily, int deliveryDays, Pair<Double, String> price,
+	               LocalDate bb) {
+		this.stock = stock;
+		this.soldDaily = soldDaily;
+		this.deliveryDays = deliveryDays;
+		this.price = price;
+		this.bb = bb;
+	}
 }
