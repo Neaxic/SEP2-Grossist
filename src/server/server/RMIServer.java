1909d434662a1f337294d718603df3f43e1d7511
Neaxic <andreas79001@gmail.com>
2021-04-28 12:32:26 +0200

    Hashmap wip

--- src/server/server/RMIServer.java
+++ src/server/server/RMIServer.java
@@ -1,11 +1,10 @@
 package server.server;
 
-import TEMP.Alcohol;
 import server.model.DataModelImpl;
 import shared.network.CallbackClient;
 import shared.network.RMIServerInterface;
 import shared.util.Util;
-import shared.wares.NewProduct;
+import shared.wares.OLD_Product;
 import shared.wares.Product;
 
 import java.rmi.AlreadyBoundException;
@@ -14,14 +13,12 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 
 public class RMIServer implements RMIServerInterface {
 	private HashMap<Integer, CallbackClient> callbackClients = new HashMap<>();
-	private HashMap<OLD_Product, Integer> wares = new HashMap<>();
-	private HashMap<Product, Integer> wares2 = new HashMap<>();
+	private HashMap<OLD_Product, Integer> wares2 = new HashMap<>();
+	private HashMap<Product, Integer> wares = new HashMap<>();
 
 	// Dummy Data:
 	private DataModelImpl dataModel = new DataModelImpl();
@@ -41,26 +38,17 @@ public class RMIServer implements RMIServerInterface {
 	}
 
 	private void getAlcohol(){
-		for (Product i : dataModel.getAlcohol())
-		{
-			//wares.put(i, dataModel.getAlcohol().size());
-		}
-		System.out.println("TEST ALCO: " +dataModel.getAlcohol());
-	}
-	private void getAllProdcts(){
-		for (Product i : dataModel.getAlcohol())
-		{
-			//wares.put(i, dataModel.getAlcohol().size());
-		}
 		System.out.println("TEST ALCO: " +dataModel.getAlcohol());
 	}
 
 	public void getAllProducts(){
 		System.out.println("HASHMAP FROM DB: " +dataModel.getAllProducts());
-		for (Object i: dataModel.getAllProducts().get("Alcohol")) {
-			wares2.put((Product) i, dataModel.getAllProducts().get("Alcohol").size());
+		//.SIZE() er forkert her...  - Vi får længden af arrayet og den indeholder
+		// alle mulige forskellige typer alkohol - ikke mængden af den spcifikke
+ 		for (Object i: dataModel.getAllProducts().get("Alcohol")) {
+			wares.put((Product) i, dataModel.getAllProducts().get("Alcohol").size());
 		}
-		System.out.println(wares2);
+		System.out.println(wares);
 	}
 
 	@Override
@@ -92,6 +80,6 @@ public class RMIServer implements RMIServerInterface {
 
 	@Override
 	public void getWares(int id) throws RemoteException { // TODO: Can overload this, creating a getWares(int id, String category)
-		callbackClients.get(id).update(wares2);
+		callbackClients.get(id).update(wares);
 	}
 }
