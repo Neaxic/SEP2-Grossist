f945f86a5440f68c14baa16efa5f9db021e7bdd9
Young <andreasyoungandersen@gmail.com>
2021-04-28 12:01:09 +0200

    Opdateret til at bruge det rigtige Product

--- src/server/model/DataModelImpl.java
+++ src/server/model/DataModelImpl.java
@@ -3,27 +3,25 @@ package server.model;
 import server.model.databaseMediator.BaseDAO;
 import server.model.databaseMediator.DAOModel;
 import server.model.databaseMediator.ModelInterface;
-import shared.wares.TestAlcohol;
+import shared.wares.Alcohol;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class DataModelImpl
-{
-  private BaseDAO base = new BaseDAO();
-  private ModelInterface model = new DAOModel();
+public class DataModelImpl {
+	private BaseDAO base = new BaseDAO();
+	private ModelInterface model = new DAOModel();
 
 
-  public DataModelImpl() throws SQLException
-  {
-  }
+	public DataModelImpl() throws SQLException {
+	}
 
-  public HashMap<String, ArrayList> getAllProducts(){
-    return model.getAllProducts();
-  }
+	public HashMap<String, ArrayList> getAllProducts() {
+	  return model.getAllProducts();
+	}
 
-  public ArrayList<TestAlcohol> getAlcohol(){
-    return model.getAlcoholProducts();
-  }
+	public ArrayList<Alcohol> getAlcohol() {
+	  return model.getAlcoholProducts();
+	}
 }
