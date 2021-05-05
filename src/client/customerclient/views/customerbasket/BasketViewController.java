31da29bbec6f56b8bf01ad89da073cea15a039dd
Neaxic <andreas79001@gmail.com>
2021-05-05 10:57:22 +0200

    BasketOverview lavet dab

--- src/client/customerclient/views/customerbasket/BasketViewController.java
+++ src/client/customerclient/views/customerbasket/BasketViewController.java
@@ -1,53 +1,86 @@
 package client.customerclient.views.customerbasket;
 
+import client.core.ViewModelFactory;
 import client.customerclient.views.CustomerViewController;
 import client.customerclient.views.CustomerViewModel;
 import client.core.ViewHandler;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.cell.PropertyValueFactory;
+import shared.wares.Product;
 
 import java.io.IOException;
 
 public class BasketViewController implements CustomerViewController {
-	@FXML private TableView basketTable;
-	private ViewHandler viewHandler;
-	private BasketViewModel viewModel;
+    @FXML
+    private TableView basketTable;
+    @FXML
+    private TableColumn basketAntal;
+    @FXML
+    private TableColumn basketProduct;
 
-	@Override
-	public void init(ViewHandler viewHandler, CustomerViewModel viewModel) {
-		this.viewHandler = viewHandler;
-		this.viewModel = (BasketViewModel) viewModel;
+    private ViewHandler viewHandler;
+    private BasketViewModel viewModel;
+    private ProductAndInt productAndInt;
+
+    private final ObservableList<Product> data =
+            FXCollections.observableArrayList();
+
+    @Override
+    public void init(ViewHandler viewHandler, CustomerViewModel viewModel) {
+        this.viewHandler = viewHandler;
+        this.viewModel = (BasketViewModel) ViewModelFactory.getInstance().basketViewModel();
+        loadAllProducts();
+    }
+
+    public void loadAllProducts() {
+        basketProduct.setCellValueFactory(new PropertyValueFactory<>("Product"));
+        basketAntal.setCellValueFactory(new PropertyValueFactory<>("Antal"));
+
+        for (Product i : viewModel.loadAllProducts().keySet()) {
+            ProductAndInt productAndInt = new ProductAndInt(i.getWareName(), viewModel.loadAllProducts().get(i));
+            basketTable.getItems().add(productAndInt);
+        }
+    }
+
+    public void removeItemFromBasket() {
+    /*
+        ObservableList<Product> list = basketTable.getSelectionModel().getSelectedItems();
+        viewModel.removeFromBasket(list);
+        basketTable.getSelectionModel().getSelectedCells().removeAll(list);
+        */
+    }
+
+    public void saveBasketToBin(ActionEvent actionEvent) {
+    }
+
+    public void SendOrder(){
+
 	}
 
-	public void removeItemFromBasket() {
-		ObservableList<Object> list = basketTable.getSelectionModel().getSelectedItems();
-		viewModel.removeFromBasket(list);
-		//test
-		basketTable.getSelectionModel().getSelectedCells().removeAll(list);
-	}
+    // SCENE MANAGING
+    @Override
+    public void swapScene(String sceneName) throws IOException {
+        viewHandler.openView(sceneName);
+    }
 
-	public void saveBasketToBin(ActionEvent actionEvent) {
-	}
+    public void openProductBrowser() throws IOException {
+        swapScene("CustomerBrowser");
+    }
 
-	// SCENE MANAGING
-	@Override
-	public void swapScene(String sceneName) throws IOException {
-		viewHandler.openView(sceneName);
-	}
+    public void openSubscriptions() throws IOException {
+        swapScene("CustomerSubscriptions");
+    }
 
-	public void openProductBrowser() throws IOException {
-		swapScene("CustomerBrowser");
-	}
-
-	public void openSubscriptions() throws IOException {
-		swapScene("CustomerSubscriptions");
-	}
-
-	public void openBasket() throws IOException {
-		swapScene("CustomerBasket");
-	}
+    public void openBasket() throws IOException {
+        swapScene("CustomerBasket");
+    }
 
 
 }
--- src/client/customerclient/views/customerbasket/BasketViewModel.java
+++ src/client/customerclient/views/customerbasket/BasketViewModel.java
@@ -1,11 +1,27 @@
 package client.customerclient.views.customerbasket;
 
 
+import client.core.ModelFactory;
+import client.customerclient.model.Model;
 import client.customerclient.views.CustomerViewModel;
 import javafx.collections.ObservableList;
+import shared.wares.Product;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class BasketViewModel implements CustomerViewModel
 {
+	private Model model;
+
+	public BasketViewModel(){
+		model = (Model) ModelFactory.getInstance().getCustomerModel();
+	}
+
 	public void removeFromBasket(ObservableList<Object> list) {
+	}
+
+	public HashMap<Product, Integer> loadAllProducts(){
+		return model.getMyBasket().getBasket();
 	}
 }
