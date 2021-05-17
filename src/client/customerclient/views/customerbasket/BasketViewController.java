514bdf8f8d497b9db2555e34c5b6045e5e285069
Young <andreasyoungandersen@gmail.com>
2021-05-17 13:06:49 +0200

    eh

--- src/client/customerclient/views/customerbasket/BasketViewController.java
+++ src/client/customerclient/views/customerbasket/BasketViewController.java
@@ -1,18 +1,15 @@
 package client.customerclient.views.customerbasket;
 
+import client.core.ViewHandler;
 import client.core.ViewModelFactory;
 import client.customerclient.views.CustomerViewController;
 import client.customerclient.views.CustomerViewModel;
-import client.core.ViewHandler;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.Tab;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.ToggleButton;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
 import shared.wares.Product;
@@ -22,84 +19,84 @@ import java.io.IOException;
 // Andreas Østergaard, Andreas Young, Line Guld
 
 public class BasketViewController implements CustomerViewController {
-    @FXML
-    private TableView basketTable;
-    @FXML
-    private TableColumn basketAntal;
-    @FXML
-    private TableColumn basketProduct;
-    @FXML
-    private Text basketSum;
+	@FXML
+	private TableView basketTable;
+	@FXML
+	private TableColumn basketAntal;
+	@FXML
+	private TableColumn basketProduct;
+	@FXML
+	private Text basketSum;
 
-    private ViewHandler viewHandler;
-    private BasketViewModel viewModel;
-    private ProductAndInt productAndInt;
+	private ViewHandler viewHandler;
+	private BasketViewModel viewModel;
+	private ProductAndInt productAndInt;
 
-    private final ObservableList<Product> data =
-            FXCollections.observableArrayList();
+	private final ObservableList<Product> data =
+			FXCollections.observableArrayList();
 
-    @Override
-    public void init(ViewHandler viewHandler, CustomerViewModel viewModel) {
-        this.viewHandler = viewHandler;
-        this.viewModel = (BasketViewModel) ViewModelFactory.getInstance().basketViewModel();
-        loadAllProducts();
-    }
+	@Override
+	public void init(ViewHandler viewHandler, CustomerViewModel viewModel) {
+		this.viewHandler = viewHandler;
+		this.viewModel = (BasketViewModel) ViewModelFactory.getInstance().basketViewModel();
+		loadAllProducts();
+	}
 
-    public void loadAllProducts() {
-        basketProduct.setCellValueFactory(new PropertyValueFactory<>("Product"));
-        basketAntal.setCellValueFactory(new PropertyValueFactory<>("Antal"));
+	public void loadAllProducts() {
+		basketProduct.setCellValueFactory(new PropertyValueFactory<>("Product"));
+		basketAntal.setCellValueFactory(new PropertyValueFactory<>("Antal"));
 
-        double price = 0;
+		double price = 0;
 
-        for (Product i : viewModel.loadAllProducts().keySet()) {
-            ProductAndInt productAndInt = new ProductAndInt(i.getWareName(), i.getWareNumber(),viewModel.loadAllProducts().get(i));
-            price += i.getPrice() * viewModel.loadAllProducts().get(i);
-            basketTable.getItems().add(productAndInt);
-        }
+		for (Product i : viewModel.loadAllProducts().keySet()) {
+			ProductAndInt productAndInt = new ProductAndInt(i.getWareName(), i.getWareNumber(), viewModel.loadAllProducts().get(i));
+			price += i.getPrice() * viewModel.loadAllProducts().get(i);
+			basketTable.getItems().add(productAndInt);
+		}
 
-        basketSum.setText("SUM: "+price +", DDK");
-    }
+		basketSum.setText("SUM: " + price + ", DDK");
+	}
 
-    public void removeItemFromBasket() {
-        Object item = basketTable.getSelectionModel().getSelectedItem(); // Item is an instance of ´ProductAndInt´
-        viewModel.removeFromBasket(item);
+	public void removeItemFromBasket() {
+		Object item = basketTable.getSelectionModel().getSelectedItem(); // Item is an instance of ´ProductAndInt´
+		viewModel.removeFromBasket(item);
+//basketTable.refresh(); // Denne metode blev tilføjet for 4 år siden til JavaFX og burde opdatere et TableView, men den virker ikke
+		//TODO: Viewet refresher ikke, efter fjerelse
 
-        //TODO: Viewet refresher ikke, efter fjerelse
+		// kunne ikke få det til at virke gliende så tager care af vigtigere ting
 
-        // kunne ikke få det til at virke gliende så tager care af vigtigere ting
+		// Men selve det at fjerne virker behind the scenes.
 
-        // Men selve det at fjerne virker behind the scenes.
+	}
 
-    }
+	public void saveBasketToBin(ActionEvent actionEvent) {
+		//TODO: (SaveBasket) Ekstra funktionallitet, slet ikke vigtig
+	}
 
-    public void saveBasketToBin(ActionEvent actionEvent) {
-        //TODO: (SaveBasket) Ekstra funktionallitet, slet ikke vigtig
-    }
-
-    public void sendOrder(){
+	public void sendOrder() {
 		viewModel.sendOrder();
 		basketTable.getItems().clear();
 		basketSum.setText("");
 		viewModel.loadAllProducts().clear();
 	}
 
-    // SCENE MANAGING
-    @Override
-    public void swapScene(String sceneName) throws IOException {
-        viewHandler.openView(sceneName);
-    }
+	// SCENE MANAGING
+	@Override
+	public void swapScene(String sceneName) throws IOException {
+		viewHandler.openView(sceneName);
+	}
 
-    public void openProductBrowser() throws IOException {
-        swapScene("CustomerBrowser");
-    }
+	public void openProductBrowser() throws IOException {
+		swapScene("CustomerBrowser");
+	}
 
-    public void openSubscriptions() throws IOException {
-        swapScene("CustomerSubscriptions");
-    }
+	public void openSubscriptions() throws IOException {
+		swapScene("CustomerSubscriptions");
+	}
 
-    public void openBasket() throws IOException {
-        swapScene("CustomerBasket");
-    }
+	public void openBasket() throws IOException {
+		swapScene("CustomerBasket");
+	}
 
 
 }
