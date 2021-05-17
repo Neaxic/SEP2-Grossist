759d7f9989a18259b1dd0343d6de2397b279d50a
Young <andreasyoungandersen@gmail.com>
2021-05-17 15:10:16 +0200

    More Tests added

--- TEST/CustomerTest.java
+++ TEST/CustomerTest.java
@@ -11,10 +11,9 @@ import org.junit.jupiter.api.Test;
 import server.server.RMIServer;
 import shared.network.RMIServerInterface;
 import shared.wares.Alcohol;
+import shared.wares.Basket;
 import shared.wares.Product;
 
-import java.rmi.AlreadyBoundException;
-import java.rmi.RemoteException;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.MissingResourceException;
@@ -23,16 +22,24 @@ import static org.junit.jupiter.api.Assertions.*;
 // Young og en overdosis af Anastasia Te. Det her kan umuligt være sundt
 
 public class CustomerTest {
-	static class CustomerBrowserTest {
-		static Model model;
+	static Product p = new Alcohol("Soplica Malinowa", "Liter", LocalDate.now(), 420, 10, 10, 10, "Poland", 30d, "Vodka");
+	static int a = 1;
+	static Model model;
+
+	@Test
+	void jUnitTest() {
+		assertTrue(false != true, "JUnit does not seem to work at the current time, please try again later...");
+	}
+
+	static class CustomerBrowserVMTest {
 		private CustomerBrowserViewModel vm;
 
 		@BeforeAll
 		static void init() {
 			try {
 				RMIServerInterface server = new RMIServer();
-				server.startServer();
-			} catch (SQLException | RemoteException | AlreadyBoundException throwable) {
+				assertDoesNotThrow(server::startServer);
+			} catch (SQLException throwable) {
 				throwable.printStackTrace();
 			}
 			ClientFactory.getInstance().getClient().start();
@@ -60,16 +67,15 @@ public class CustomerTest {
 		}
 	}
 
-	static class CustomerBasketTest {
-		static Model model;
+	static class CustomerBasketVMTest {
 		private BasketViewModel vm;
 
 		@BeforeAll
 		static void init() {
 			try {
 				RMIServerInterface server = new RMIServer();
-				server.startServer();
-			} catch (SQLException | RemoteException | AlreadyBoundException throwable) {
+				assertDoesNotThrow(server::startServer);
+			} catch (SQLException throwable) {
 				throwable.printStackTrace();
 			}
 			ClientFactory.getInstance().getClient().start();
@@ -83,8 +89,6 @@ public class CustomerTest {
 
 		@Test
 		void removeFromBasket() {
-			Product p = new Alcohol("Soplica Malinowa", "Liter", LocalDate.now(), 420, 10, 10, 10, "Poland", 30d, "Vodka");
-			int a = 1;
 			model.addToBasket(p, a);
 			assertFalse(model.getMyBasket().getBasket().isEmpty());
 			vm.removeFromBasket(new ProductAndInt(p.getWareName(), p.getWareNumber(), a));
@@ -95,8 +99,6 @@ public class CustomerTest {
 		void sendOrder() {
 			LoginManager.cvr = 1;
 			assertThrows(MissingResourceException.class, () -> vm.sendOrder());
-			Product p = new Alcohol("Soplica Malinowa", "Liter", LocalDate.now(), 420, 10, 10, 10, "Poland", 30d, "Vodka");
-			int a = 1;
 			model.addToBasket(p, a);
 			assertDoesNotThrow(() -> vm.sendOrder());
 		}
@@ -104,6 +106,83 @@ public class CustomerTest {
 		@Test
 		void loadAllProducts() {
 			assertEquals(model.getMyBasket().getBasket(), vm.loadAllProducts());
+		}
+	}
+
+	static class CustomerModelTest {
+		@BeforeAll
+		static void init() {
+			try {
+				RMIServerInterface server = new RMIServer();
+				assertDoesNotThrow(server::startServer);
+			} catch (SQLException throwable) {
+				throwable.printStackTrace();
+			}
+			ClientFactory.getInstance().getClient().start();
+		}
+
+		@BeforeEach
+		void setUp() {
+			model = (Model) ModelFactory.getInstance().getCustomerModel();
+		}
+
+		@Test
+		void addToBasket() {
+			assertTrue(model.getMyBasket().getBasket().isEmpty());
+			assertDoesNotThrow(() -> model.addToBasket(p, a));
+			assertFalse(model.getMyBasket().getBasket().isEmpty());
+		}
+
+		@Test
+		void removeFromBasket() {
+			assertDoesNotThrow(() -> model.removeFromBasket(p)); // Documentation does not state, it shall throw an Error when the basket is empty and you attempt to remove an item from it
+			model.addToBasket(p, a);
+			assertFalse(model.getMyBasket().getBasket().isEmpty());
+			assertDoesNotThrow(() -> model.removeFromBasket(p));
+			assertTrue(model.getMyBasket().getBasket().isEmpty());
+		}
+
+		@Test
+		void changeAmount() {
+			int n = 2;
+			model.addToBasket(p, a);
+			assertDoesNotThrow(() -> model.changeAmount(p, n));
+			assertEquals(n, model.getMyBasket().getAmount(p));
+		}
+
+		@Test
+		void updateWares() { // Unsure of how we should go about testing this
+			assertDoesNotThrow(() -> model.updateWares());
+			assertFalse(model.getAllWares().isEmpty());
+		}
+
+		@Test
+		void getMyBasket() {
+			assertNotNull(model.getMyBasket());
+			assertTrue(model.getMyBasket().getBasket().isEmpty());
+			model.addToBasket(p, a);
+			assertFalse(model.getMyBasket().getBasket().isEmpty());
+		}
+
+		@Test
+		void sendOrder() {
+			assertThrows(MissingResourceException.class, () -> model.sendOrder(model.getMyBasket(), 10));
+			model.addToBasket(p, a);
+			assertDoesNotThrow(() -> model.sendOrder(model.getMyBasket(), 10));
+		}
+
+		@Test
+		void getAllWares() {
+			assertTrue(model.getAllWares().isEmpty());
+			model.updateWares();
+			assertFalse(model.getAllWares().isEmpty());
+		}
+
+		@Test
+		void getCategory() {
+			assertTrue(model.getCategory("Drink").isEmpty());
+			model.updateWares();
+			assertTrue(model.getAllWares().containsAll(model.getCategory("Drink")));
 		}
 	}
 }
