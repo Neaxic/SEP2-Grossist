36323a955641f6c80d3804eb87149f08387c0641
Young <andreasyoungandersen@gmail.com>
2021-05-18 22:54:13 +0200

    Skaber fejl, Andreas vil fikse det inden i løbet af dagen (kan godt være jeg ender med at hjælpe ham)

--- src/server/model/databaseMediator/ModelInterface.java
+++ src/server/model/databaseMediator/ModelInterface.java
@@ -1,5 +1,6 @@
 package server.model.databaseMediator;
 
+import javafx.util.Pair;
 import shared.wares.Basket;
 import shared.wares.Order;
 import shared.wares.Product;
@@ -11,14 +12,18 @@ import java.util.HashMap;
 
 // Lavet af hele teamet
 
-public interface ModelInterface
-{
-  HashMap<String, ArrayList<Product>> getAllProducts();
-  ArrayList<Order> getAllOrders();
-  ArrayList<Product> getProduct(String schemaName, String productClass); //TEMP FIX
-  void createOrder(int cvr, double sum, LocalDate date);
-  void createOrderSpec(Basket basket, int CVR, LocalDate date, double sum) throws
-      SQLException;
-  int getProductAmountInStockFromProductId(int id) throws SQLException;
-  void CreateProduct(Product newProduct);
+public interface ModelInterface {
+	HashMap<String, ArrayList<Product>> getAllProducts();
+
+	ArrayList<Order> getAllOrders();
+
+	ArrayList<Product> getProduct(String schemaName, String productClass); //TEMP FIX
+
+	void createOrder(int cvr, double sum, LocalDate date);
+
+	void createOrderSpec(Basket basket, int CVR, LocalDate date, double sum) throws SQLException;
+
+	int getProductAmountInStockFromProductId(int id) throws SQLException;
+
+	void CreateProduct(Pair<Product, Integer> newProduct) throws SQLException;
 }
