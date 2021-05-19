5443dd0347cd080bb972dc7e9eaeab2153a44217
Andre <andreas79001@gmail.com>
2021-05-19 11:51:11 +0200

    fikset fejl

--- src/client/grosserclient/model/GrosserModelInterface.java
+++ src/client/grosserclient/model/GrosserModelInterface.java
@@ -1,10 +1,11 @@
 package client.grosserclient.model;
 
+import javafx.util.Pair;
 import shared.network.Subject;
 import shared.wares.Product;
 
 public interface GrosserModelInterface extends Subject
 {
-    public void createNewProduct(Product newProduct);
+    public void createNewProduct(Pair<Product, Integer> newProduct);
   void getAllOrders();
 }
