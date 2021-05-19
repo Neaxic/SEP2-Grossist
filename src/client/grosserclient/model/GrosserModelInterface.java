708fda3d34f4da2eccffe83b71de34c19185116f
Young <andreasyoungandersen@gmail.com>
2021-05-19 12:43:49 +0200

    Formatering

--- src/client/grosserclient/model/GrosserModelInterface.java
+++ src/client/grosserclient/model/GrosserModelInterface.java
@@ -4,8 +4,8 @@ import javafx.util.Pair;
 import shared.network.Subject;
 import shared.wares.Product;
 
-public interface GrosserModelInterface extends Subject
-{
-    public void createNewProduct(Pair<Product, Integer> newProduct);
-  void getAllOrders();
+public interface GrosserModelInterface extends Subject {
+	void createNewProduct(Pair<Product, Integer> newProduct);
+
+	void getAllOrders();
 }
